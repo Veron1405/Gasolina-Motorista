161492f0f98a7514799f08d90be603c6332d3b23
Veron1405 <leonardo.verondias1405@gmail.com>
2024-08-28 14:14:32 -0300

    Initial commit

---
+++ src/views/gasolinamotorista.js
@@ -0,0 +1,217 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './gasolinamotorista.css'
+
+const GASOLINAMOTORISTA = (props) => {
+  return (
+    <div className="gasolinamotorista-container">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="gasolinamotoristagasolinamotorista">
+        <img
+          src="/external/ellipse413-nnpc-400h.png"
+          alt="Ellipse413"
+          className="gasolinamotorista-ellipse4"
+        />
+        <img
+          src="/external/ellipse1314-ft6-200h.png"
+          alt="Ellipse1314"
+          className="gasolinamotorista-ellipse13"
+        />
+        <div className="gasolinamotorista-back-ground">
+          <img
+            src="/external/ellipse116-5v2-400w.png"
+            alt="Ellipse116"
+            className="gasolinamotorista-ellipse1"
+          />
+          <img
+            src="/external/ellipse217-see-400w.png"
+            alt="Ellipse217"
+            className="gasolinamotorista-ellipse2"
+          />
+          <img
+            src="/external/ellipse318-ifs-400w.png"
+            alt="Ellipse318"
+            className="gasolinamotorista-ellipse3"
+          />
+          <img
+            src="/external/ellipse819-xyin-200h.png"
+            alt="Ellipse819"
+            className="gasolinamotorista-ellipse8"
+          />
+          <img
+            src="/external/ellipse15110-y0ue-200h.png"
+            alt="Ellipse15110"
+            className="gasolinamotorista-ellipse15"
+          />
+          <img
+            src="/external/ellipse14111-7eb-300h.png"
+            alt="Ellipse14111"
+            className="gasolinamotorista-ellipse14"
+          />
+        </div>
+        <img
+          src="/external/ellipse6112-iomd-200h.png"
+          alt="Ellipse6112"
+          className="gasolinamotorista-ellipse6"
+        />
+        <span className="gasolinamotorista-text10">
+          <span>
+            Gastos e consumo!
+            <span
+              dangerouslySetInnerHTML={{
+                __html: ' ',
+              }}
+            />
+          </span>
+        </span>
+        <img
+          src="/external/ellipse12114-rpuk-200h.png"
+          alt="Ellipse12114"
+          className="gasolinamotorista-ellipse12"
+        />
+        <img
+          src="/external/rectangle15115-idwh-400w.png"
+          alt="Rectangle15115"
+          className="gasolinamotorista-rectangle15"
+        />
+        <img
+          src="/external/rectangle20116-ejnaf-200h.png"
+          alt="Rectangle20116"
+          className="gasolinamotorista-rectangle20"
+        />
+        <img
+          src="/external/rectangle25117-88i-200h.png"
+          alt="Rectangle25117"
+          className="gasolinamotorista-rectangle25"
+        />
+        <img
+          src="/external/rectangle29118-af9w-200h.png"
+          alt="Rectangle29118"
+          className="gasolinamotorista-rectangle29"
+        />
+        <img
+          src="/external/rectangle22119-h9k-200h.png"
+          alt="Rectangle22119"
+          className="gasolinamotorista-rectangle22"
+        />
+        <img
+          src="/external/rectangle26120-l7q-200h.png"
+          alt="Rectangle26120"
+          className="gasolinamotorista-rectangle26"
+        />
+        <img
+          src="/external/rectangle30121-03mh-200h.png"
+          alt="Rectangle30121"
+          className="gasolinamotorista-rectangle30"
+        />
+        <img
+          src="/external/rectangle31122-84jk-200h.png"
+          alt="Rectangle31122"
+          className="gasolinamotorista-rectangle31"
+        />
+        <img
+          src="/external/rectangle32123-urg-200h.png"
+          alt="Rectangle32123"
+          className="gasolinamotorista-rectangle32"
+        />
+        <span className="gasolinamotorista-text12">
+          <span>Últimos abastecimentos:</span>
+        </span>
+        <span className="gasolinamotorista-text14">
+          <span>Valores</span>
+        </span>
+        <span className="gasolinamotorista-text16">
+          <span>Data</span>
+        </span>
+        <span className="gasolinamotorista-text18">
+          <span>R$ 150</span>
+        </span>
+        <span className="gasolinamotorista-text20">
+          <span>R$ 50</span>
+        </span>
+        <span className="gasolinamotorista-text22">
+          <span>R$ 120</span>
+        </span>
+        <span className="gasolinamotorista-text24">
+          <span>R$ 80</span>
+        </span>
+        <span className="gasolinamotorista-text26">
+          <span>R$ 110</span>
+        </span>
+        <span className="gasolinamotorista-text28">
+          <span>R$ 110</span>
+        </span>
+        <span className="gasolinamotorista-text30">
+          <span>R$ 90</span>
+        </span>
+        <span className="gasolinamotorista-text32">
+          <span>R$ 60</span>
+        </span>
+        <span className="gasolinamotorista-text34">
+          <span>09/06/2024</span>
+        </span>
+        <span className="gasolinamotorista-text36">
+          <span>21/05/2024</span>
+        </span>
+        <span className="gasolinamotorista-text38">
+          <span>31/06/2024</span>
+        </span>
+        <span className="gasolinamotorista-text40">
+          <span>18/05/2024</span>
+        </span>
+        <span className="gasolinamotorista-text42">
+          <span>28/05/2024</span>
+        </span>
+        <span className="gasolinamotorista-text44">
+          <span>14/05/2024</span>
+        </span>
+        <span className="gasolinamotorista-text46">
+          <span>25/05/2024</span>
+        </span>
+        <span className="gasolinamotorista-text48">
+          <span>11/05/2024</span>
+        </span>
+        <div className="gasolinamotorista-header">
+          <img
+            src="/external/rectangle7144-7l3.svg"
+            alt="Rectangle7144"
+            className="gasolinamotorista-rectangle7"
+          />
+          <div className="gasolinamotorista-group">
+            <img
+              src="/external/vector146-sepg.svg"
+              alt="Vector146"
+              className="gasolinamotorista-vector1"
+            />
+            <img
+              src="/external/vector147-piv.svg"
+              alt="Vector147"
+              className="gasolinamotorista-vector2"
+            />
+          </div>
+          <span className="gasolinamotorista-text50">
+            <span>
+              Gasolina
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+          </span>
+          <img
+            src="/external/vector149-u28.svg"
+            alt="Vector149"
+            className="gasolinamotorista-vector3"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default GASOLINAMOTORISTA
